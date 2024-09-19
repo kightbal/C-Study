e9cc203258a8958e25a6b7ce1b02567637b38c0d
Jinke Yu <a1397577122@gmail.com>
2024-09-19 11:18:17 +0800

    challenge14 friend function implementation operator

--- Section14/Challenge/Mystring.cpp
+++ Section14/Challenge/Mystring.cpp
@@ -92,10 +92,112 @@ std::istream &operator>>(std::istream &in, Mystring &rhs) {
     return in;
 }
 
-Mystring Mystring::operator-() {
-    char *buff = new char[std::strlen(str) + 1];
-    buff = strcpy(buff, str);
-    for (size_t i = 0; i < std::strlen(str); i++) {
+/*
+
+   使用成员函数重载操作符，包括-,==,!=,<,,+,+=,*,*=,++Mystring,Mystring++等操作符。
+    Use member functions to overload operators, including -, ==, !=, <, +, +=, *, *=, ++Mystring, Mystring++.
+*/
+
+//重载操作符-,作用是将字符串小写
+// Overload the operator- to convert the string to lowercase
+// Mystring Mystring::operator-() {
+//     char *buff = new char[std::strlen(str) + 1];
+//     buff = strcpy(buff, str);
+//     for (size_t i = 0; i < std::strlen(str); i++) {
+//         buff[i] = std::tolower(buff[i]);
+//     }
+//     Mystring temp{buff};
+//     delete[] buff;
+//     return temp;
+// }
+
+//重载操作符==，作用是判断字符串是否完全相等
+// Overload the operator== to check if the strings are equal
+// bool Mystring::operator==(const Mystring &ref) {
+//     size_t size_ref = std::strlen(ref.str);
+//     size_t size_own = std::strlen(str);
+//     if (size_ref != size_own)
+//         return false;
+//     for (size_t i = 0; i < size_ref; i++) {
+//         if (ref.str[i] != str[i])
+//             return false;
+//     }
+//     return true;
+// }
+
+//重载操作符!=,作用是判断字符串是否不完全相等
+// Overload the operator!= to check if the strings are not equal
+// bool Mystring::operator!=(const Mystring &ref) {
+//     size_t size_ref = std::strlen(ref.str);
+//     size_t size_own = std::strlen(str);
+//     if (size_ref != size_own)
+//         return true;
+//     for (size_t i = 0; i < size_ref; i++) {
+//         if (ref.str[i] != str[i])
+//             return true;
+//     }
+//     return false;
+// }
+
+//重载操作符<,根据strcmp来比较字符串是否小于ref,小于则返回true
+// Overload the operator< to compare if the string is less than ref using strcmp, return true if less
+// bool Mystring::operator<(const Mystring &ref) {
+//     return strcmp(str, ref.str) < 0 ? true : false;
+// }
+
+//重载操作符>,根据strcmp来比较字符串是否大于ref,大于则返回true
+// Overload the operator> to compare if the string is greater than ref using strcmp, return true if greater
+// bool Mystring::operator>(const Mystring &ref) {
+//     return strcmp(str, ref.str) > 0 ? true : false;
+// }
+
+//重载操作符+,将两字符串相加
+// Overload the operator+ to concatenate two strings
+// Mystring Mystring::operator+(const Mystring &ref) {
+//     char *buff = new char[std::strlen(ref.str) + std::strlen(str) + 1];
+//     std::strcpy(buff, str);
+//     std::strcat(buff, ref.str);
+//     Mystring temp = Mystring{buff};
+//     delete[] buff;
+//     return temp;
+// }
+
+//重载操作符+=,将字符串相加并赋值给当前字符串
+// Overload the operator+= to concatenate the string and assign it to the current string
+// void Mystring::operator+=(const Mystring &ref) {
+//     *this = *this + ref;
+//     return;
+// }
+
+//重载操作符*,将字符串乘以一个数字
+// Overload the operator* to multiply the string by a number
+// Mystring Mystring::operator*(int count) {
+//     char *buff = new char[std::strlen(str) * count + 1];
+//     std::strcpy(buff, str);
+//     for (int i = 0; i < count - 1; i++) {
+//         std::strcat(buff, str);
+//     }
+//     Mystring temp{buff};
+//     delete[] buff;
+//     return temp;
+// }
+
+// void Mystring::operator*=(int count) {
+//     *this = *this * count;
+//     return;
+// }
+
+/*
+
+   使用friend函数重载操作符，包括-,==,!=,<,,+,+=,*,*=,++Mystring,Mystring++等操作符。
+    Use friend functions to overload operators, including -, ==, !=, <, +, +=, *, *=, ++Mystring, Mystring++.
+*/
+//重载操作符-,作用是将字符串小写
+// Overload the operator- to convert the string to lowercase
+Mystring operator-(const Mystring &ref) {
+    char *buff = new char[std::strlen(ref.str) + 1];
+    buff = strcpy(buff, ref.str);
+    for (size_t i = 0; i < std::strlen(ref.str); i++) {
         buff[i] = std::tolower(buff[i]);
     }
     Mystring temp{buff};
@@ -103,62 +205,91 @@ Mystring Mystring::operator-() {
     return temp;
 }
 
-bool Mystring::operator==(const Mystring &ref) {
-    size_t size_ref = std::strlen(ref.str);
-    size_t size_own = std::strlen(str);
-    if (size_ref != size_own)
+//重载操作符==，作用是判断字符串是否完全相等
+// Overload the operator== to check if the strings are equal
+bool operator==(const Mystring &lref, const Mystring &rref) {
+    size_t size_lref = std::strlen(lref.str);
+    size_t size_rref = std::strlen(rref.str);
+    if (size_lref != size_rref)
         return false;
-    for (size_t i = 0; i < size_ref; i++) {
-        if (ref.str[i] != str[i])
+    for (size_t i = 0; i < size_lref; i++) {
+        if (lref.str[i] != rref.str[i])
             return false;
     }
     return true;
 }
 
-bool Mystring::operator!=(const Mystring &ref) {
-    size_t size_ref = std::strlen(ref.str);
-    size_t size_own = std::strlen(str);
-    if (size_ref != size_own)
-        return true;
-    for (size_t i = 0; i < size_ref; i++) {
-        if (ref.str[i] != str[i])
-            return true;
-    }
-    return false;
+//重载操作符!=,作用是判断字符串是否不完全相等
+// Overload the operator!= to check if the strings are not equal
+bool operator!=(const Mystring &lref, const Mystring &ref) {
+    return !(lref == ref);
 }
 
-bool Mystring::operator<(const Mystring &ref) {
-    return strcmp(str, ref.str) < 0 ? true : false;
-}
-bool Mystring::operator>(const Mystring &ref) {
-    return strcmp(str, ref.str) > 0 ? true : false;
+//重载操作符<,根据strcmp来比较字符串是否小于ref,小于则返回true
+// Overload the operator< to compare if the string is less than ref using strcmp, return true if less
+bool operator<(const Mystring &lref, const Mystring &ref) {
+    return strcmp(lref.str, ref.str) < 0 ? true : false;
 }
 
-Mystring Mystring::operator+(const Mystring &ref) {
-    char *buff = new char[std::strlen(ref.str) + std::strlen(str) + 1];
-    std::strcpy(buff, str);
+//重载操作符>,根据strcmp来比较字符串是否大于ref,大于则返回true
+// Overload the operator> to compare if the string is greater than ref using strcmp, return true if greater
+bool operator>(const Mystring &lref, const Mystring &ref) {
+    return !(lref < ref);
+}
+
+//重载操作符+,将两字符串相加
+// Overload the operator+ to concatenate two strings
+Mystring operator+(const Mystring &lref, const Mystring &ref) {
+    char *buff = new char[std::strlen(ref.str) + std::strlen(lref.str) + 1];
+    std::strcpy(buff, lref.str);
     std::strcat(buff, ref.str);
     Mystring temp = Mystring{buff};
     delete[] buff;
     return temp;
 }
 
-void Mystring::operator+=(const Mystring &ref) {
-    *this = *this + ref;
+//重载操作符+=,将字符串相加并赋值给当前字符串
+// Overload the operator+= to concatenate the string and assign it to the current string
+void operator+=(Mystring &lref, const Mystring &ref) {
+    lref = lref + ref;
     return;
 }
 
-Mystring Mystring::operator*(int count) {
-    char *buff = new char[std::strlen(str) * count + 1];
-    std::strcpy(buff, str);
+//重载操作符*,将字符串乘以一个数字
+// Overload the operator* to multiply the string by a number
+Mystring operator*(const Mystring &ref, int count) {
+    char *buff = new char[std::strlen(ref.str) * count + 1];
+    std::strcpy(buff, ref.str);
     for (int i = 0; i < count - 1; i++) {
-        std::strcat(buff, str);
+        std::strcat(buff, ref.str);
     }
     Mystring temp{buff};
     delete[] buff;
     return temp;
 }
-void Mystring::operator*=(int count) {
-    *this = *this * count;
+
+//重载操作符+=,将字符串乘以一个数字并赋值给当前字符串
+void operator*=(Mystring &ref, int count) {
+    ref = ref * count;
     return;
+}
+
+//重载操作符前++，将当前字符串大写，返回为大写前的字符串
+Mystring &operator++(Mystring &ref) {
+    // do what ever you want increment do to - maybe make all characters uppercase?
+    char *buff = new char[std::strlen(ref.str) + 1];
+    std::strcpy(buff, ref.str);
+    for (size_t i = 0; i < std::strlen(ref.str); i++) {
+        buff[i] = std::toupper(buff[i]);
+    }
+    ref = Mystring(buff);
+    delete[] buff;
+    return ref;
+}
+
+//重载操作符后++,将当前字符串大写并返回大写后的字符串
+Mystring operator++(Mystring &ref, int) {
+    Mystring temp(ref); // make a copy
+    ++(ref);            // call pre-increment - make sure you call pre-increment!
+    return temp;        // return the old value
 }
--- Section14/Challenge/Mystring.h
+++ Section14/Challenge/Mystring.h
@@ -23,31 +23,42 @@ public:
     int get_length() const; // getters
     const char *get_str() const;
 
-    Mystring operator-();
-    bool operator==(const Mystring &ref);
-    bool operator!=(const Mystring &ref);
-    bool operator<(const Mystring &ref);
-    bool operator>(const Mystring &ref);
-    Mystring operator+(const Mystring &ref);
-    void operator+=(const Mystring &ref);
-    Mystring operator*(int count);
-    void operator*=(int count);
-    Mystring &operator++() { // pre-increment
-        // do what ever you want increment do to - maybe make all characters uppercase?
-        char *buff = new char[std::strlen(str) + 1];
-        std::strcpy(buff, str);
-        for (size_t i = 0; i < std::strlen(str); i++) {
-            buff[i] = std::toupper(buff[i]);
-        }
-        *this = Mystring(buff);
-        delete[] buff;
-        return *this;
-    }
-    Mystring operator++(int) { // post-increment
-        Mystring temp(*this);  // make a copy
-        ++(*this);             // call pre-increment - make sure you call pre-increment!
-        return temp;           // return the old value
-    }
+    // Mystring operator-();
+    // bool operator==(const Mystring &ref);
+    // bool operator!=(const Mystring &ref);
+    // bool operator<(const Mystring &ref);
+    // bool operator>(const Mystring &ref);
+    // Mystring operator+(const Mystring &ref);
+    // void operator+=(const Mystring &ref);
+    // Mystring operator*(int count);
+    // void operator*=(int count);
+    // Mystring &operator++() { // pre-increment
+    //     // do what ever you want increment do to - maybe make all characters uppercase?
+    //     char *buff = new char[std::strlen(str) + 1];
+    //     std::strcpy(buff, str);
+    //     for (size_t i = 0; i < std::strlen(str); i++) {
+    //         buff[i] = std::toupper(buff[i]);
+    //     }
+    //     *this = Mystring(buff);
+    //     delete[] buff;
+    //     return *this;
+    // }
+    // Mystring operator++(int) { // post-increment
+    //     Mystring temp(*this);  // make a copy
+    //     ++(*this);             // call pre-increment - make sure you call pre-increment!
+    //     return temp;           // return the old value
+    // }
+    friend Mystring operator-(const Mystring &ref);
+    friend bool operator==(const Mystring &lref, const Mystring &ref);
+    friend bool operator!=(const Mystring &lref, const Mystring &ref);
+    friend bool operator<(const Mystring &lref, const Mystring &ref);
+    friend bool operator>(const Mystring &lref, const Mystring &ref);
+    friend Mystring operator+(const Mystring &lref, const Mystring &ref);
+    friend void operator+=(Mystring &lref, const Mystring &ref);
+    friend Mystring operator*(const Mystring &ref, int count);
+    friend void operator*=(Mystring &ref, int count);
+    friend Mystring &operator++(Mystring &ref);
+    friend Mystring operator++(Mystring &ref, int);
 };
 
 #endif // _MYSTRING_H_
